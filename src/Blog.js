d2107b98b79249e9ab885e7d3b59faef903492bb
Sasikumar00 <sasikumarworks@gmail.com>
2025-07-16 20:26:40 +0530

    feat(blogpost): add image string field for blogs

--- src/Blog.js
+++ src/Blog.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
 const blogpostSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    body: {
-        type: String,
-        required: true
-    }
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
 });
 
-export default mongoose.model('Blog', blogpostSchema);
+export default mongoose.model("Blog", blogpostSchema);
--- src/blogRoutes.js
+++ src/blogRoutes.js
@@ -4,27 +4,28 @@ import Blog from "./Blog.js";
 const router = Router();
 
 router.get("/", async (req, res) => {
-    const blogs = await Blog.find();
-    res.json(blogs);
+  const blogs = await Blog.find();
+  res.json(blogs);
 });
 
 router.post("/", async (req, res) => {
-    const blog = new Blog(req.body);
-    await blog.save();
-    res.json(blog);
+  const blog = new Blog(req.body);
+  await blog.save();
+  res.json(blog);
 });
 
 router.get("/:slug", async (req, res) => {
-    const blog = await Blog.findOne({ title: req.params.slug });
-    res.json(blog);
+  const blog = await Blog.findOne({ title: req.params.slug });
+  res.json(blog);
 });
 
 router.put("/:slug", async (req, res) => {
-    const blog = await Blog.findOne({ title: req.params.slug });
-    blog.title = req.body.title;
-    blog.body = req.body.body;
-    await blog.save();
-    res.json(blog);
+  const blog = await Blog.findOne({ title: req.params.slug });
+  blog.title = req.body.title;
+  blog.body = req.body.body;
+  blog.image = req.body.image;
+  await blog.save();
+  res.json(blog);
 });
 
 export default router;
